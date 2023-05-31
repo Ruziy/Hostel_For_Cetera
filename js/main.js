4aba089b19e51d09ab48f34feee596be178e263f
Ruziy <107850684+Ruziy@users.noreply.github.com>
2023-06-01 00:05:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.main__slider').slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        dots:false,
+        arrows:true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true
+    });
+})
